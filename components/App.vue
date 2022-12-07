283e7dc9460af8f586f6a14e8cf04357fa18089c
starikovyaroslav <slava.com.com90@gmail.com>
2022-12-07 21:59:37 +0700

    fix: запушил недостающие файлы

---
+++ components/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app">
+    <Sidebar />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <App />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
